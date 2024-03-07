8cdc952d3e20416f6f223ace5a9f446de175d69e
Iqra Jahangir <iqrasheikh1436@gmail.com>
2024-03-07 16:49:32 +0500

    first commit

---
+++ cal.py
@@ -0,0 +1,38 @@
+choice = int(input("choose 1 from(1-5)"))
+
+if choice == 1 : 
+ a = int(input("ENTER YOUR FIRST VALUE"))
+ b = int(input("ENTER YOUR SECOND VALUE"))
+ result =  a+b
+ print("ADDITION:", result)
+
+
+elif choice == 2 : 
+ a = int(input("enter your first value"))
+ b = int(input("enter your second value"))
+ result =  a-b
+ print("SUBTRACTION:", result)
+
+elif choice == 3 : 
+ a = int(input("enter your first value"))
+ b = int(input("enter your second value"))
+ result =  a*b
+ print("MULTIPLICATION:", result)
+
+elif choice == 4 : 
+ a = int(input("enter your first value"))
+ b = int(input("enter your second value"))
+ result =  a*b
+ print("MULTIPLICATION:", result)
+
+elif choice == 5 : 
+ a = int(input("enter your first value"))
+ b = int(input("enter your second value"))
+ result =  a%b
+ print("MODULES:", result)
+
+
+
+
+
+
---
+++ index.py
@@ -0,0 +1,337 @@
+# PYTHON 
+# PYTHON IS A PROGRAMMING LANGUAGE 
+# PYTHON IS INTERPRETED
+# ITS EASY TO LEARN AND EASY TO UNDERSTAND SIMILER TO ENGLISH
+# PYTHON IS CASE SENSITIVE
+# PYTHON USE TO STATIC DATA SCEINCE 
+# Python is a popular programming language.
+# Python can be used on a server to create web applications.
+# Python is an interpreted programming language, this means that as a developer you write Python (.py) files in a text editor and then put those files into the python interpreter to be executed.
+# The way to run a python file is like this on the command line:
+
+# INSTALLATION 
+# 1. INSTALL PYTHION FROM GOOGLE 
+# 2. INTSALL FROM APP STORE 
+# 3. INSTALL VS CODE FROM GOOGLE
+
+# VARIABLES 
+# VARIABLE IS A LOCATION WHERE WE STORE OUR DATA 
+# var = "Naeem"
+
+# DATA TYPES OF PYHTON 
+
+# # NUMERIC  
+# var1 = 3847
+# print('\nhello I am integer', var1)
+
+
+# # STRING
+# var2 = "double"
+# var3 = 'Single'
+# print('\n hello I am string with ', var2, ' couts')
+# print('\n hello I am string with ',var3, 'couts')
+
+
+# # BOOLEAN 
+# var4 = True
+# var5 = False
+# print('\nhello I am boolean with ', var4)
+# print('\nhello I am boolean with', var5)
+
+# # FLOAT
+# var6 = 66.6
+# print('\nhello I am float with', var6)
+
+# COLLECTION TYPES
+# 1. list(Emuted = Changeable)
+# list1 = ["String with double cots",'String with Single cots',"'String with Single cots'",1010,True,False,99.9]
+# print("this is list",list1)
+
+# # 2. tuple(muted = Unchangeable)
+# Tup = ("String with double cots",'String with Single cots',1010,True,False,99.9)
+# print("this is tuple",Tup)
+
+# # 3. dict (keys and value pairs like an object)
+# dict = {1:55,2:45,3:35,4:66}
+# print("this is dict",dict)
+
+# # 4. sets 
+# set = {44,66,88}
+# print("this is set",set)
+
+# OPERATORS
+# Arithmetic operators
+
+# num1 = int(input('enter your first value: '))
+# num2 =  int(input('enter your second value: '))
+# result = num1 + num2
+# print('ADDITION : ' , result)
+
+# result = num1 - num2
+# print('SUBTRACTION: ' , result)
+
+# result = num1 * num2
+# print('MULTIPLICATION: ' , result)
+
+# result = num1 / num2
+# print('DIVISION: ' , result)
+
+# result = num1 % num2
+# print('MODULES: ' , result)
+
+# result = num1 ** num2
+# print('EXPONENT: ' , result)
+
+
+# ASSIGNEMNT OPERATOR 
+
+# num = 10
+# print('Origional value ',num)
+
+# num = num + 10 
+# print('new value with normal operators ',num)
+
+# num+= 5
+# print('New Value with assign operators (+=) ',num)
+
+
+
+# num1 = 50
+# print('Origional value ',num1)
+
+# num1 = num1 - 10 
+# print('new value with normal operators ',num1)
+
+# num1-= 5
+# print('New Value with assign operators (-=) ',num1)
+
+
+# num1 = 50
+# num1 = 50+2
+# num1+= 2
+# print('Value of Add assignment operator',num1)
+
+# num2 = 50
+# num2 = 50-2
+# num2-= 2
+# print('Value of sub assignment operator',num2)
+
+
+# num3 = 50
+# num3 = 50*2
+# num3*= 2
+# print('Value of multiply assignment operator',num3)
+
+
+# num4 = 50
+# num4 = 50/2
+# num4/= 5
+# print('Value of div assignment operator',num4)
+
+# num5 = 50
+# num5 = 50%2
+# num5%= 5
+# print('Value of modules assignment operator',num5)
+
+
+# num6 = 10
+# num6 = num4**2
+# num6**= 2
+# print('Value of exponent assignment operator',num6)
+
+# == Assignment operator 
+# no1 = 5 
+# no2 = 10
+# print("Check", no1,"and" ,no2, "is equal to or not: " ,no1==no2 )
+# print("Check", no1,"and" ,no2, "is not equal to or not: " ,no1!=no2 )
+# print("Check", no1," is greater than" ,no2, "or not: " ,no1>no2 )
+# print("Check", no1," is less than" ,no2, "or not: " ,no1<no2 ) 
+# print("Check", no1," is greater than and equal to" ,no2, "or not: " ,no1>=no2 ) 
+# print("Check", no1," is less than and equal to" ,no2, "or not: " ,no1<=no2 ) 
+
+
+
+# value1 = True 
+# value2 = False 
+# print("check condition", value1, " and ", value2 , "condition::" ,value1 and value2 )
+
+# no1= 10
+# no2= 5
+# print("check condition", no1, " and ", no2 , "condition:" ,no1 == 10  and  no2 == 5)
+
+# no1= 10
+# no2= 5
+# print("check condition", no1, " and ", no2 , "condition:" ,no1 >= 5  and  no2 <= 10)
+
+# no1= 10
+# no2= 5
+# print("check condition", no1, " and ", no2 , "condition:" ,no1 <= 10  and  no2 <= 5)
+
+# no1= 10
+# no2= 5
+# print("check condition", no1, " and ", no2 , "condition:" ,no1 != 5  and  no2 != 10)
+
+
+# print('1 => Addition: ')
+# print('2 => Subtracton; ')
+# print('3 => multipilication: ')
+# print('4 => Division: ')
+
+# choices = int(input("please select an operator from(1 to 4) for calculate "))
+
+# if choices == 1 :
+#     No1 = int(input("please enter your first value: ")) 
+#     No2 = int(input("please enter your first value: ")) 
+#     result = No1+No2
+#     print(No1,"+",No2,"=", result)
+# else:
+
+
+
+#     else choices == 1 :
+#     No1 = int(input("please enter your first value: ")) 
+#     No2 = int(input("please enter your first value: ")) 
+#     result = No1+No2
+
+# print(No1,"+",No2,"=", result)
+
+    # else
+
+# no1 = int(input('please enter your No1'))
+# no2 = int(input('please enter your No2'))
+# print(no1," + ",no2, " = ", "Same? ", no1 and no2)
+
+# no1 = int(input('please enter your No1'))
+# no2 = int(input('please enter your No2'))
+# print(no1," - ",no2, " = ", no1+no2 )
+
+# no1 = int(input('please enter your No1'))
+# no2 = int(input('please enter your No2'))
+# print(no1," * ",no2, " = ", no1+no2 )
+
+# no1 = int(input('please enter your No1'))
+# no2 = int(input('please enter your No2'))
+# print(no1," / ",no2, " = ", no1+no2 )
+
+# no1 = int(input('please enter your No1'))
+# # no2 = int(input('please enter your No2'))
+# # print(no1," % ",no2, " = ", no1+no2 )
+
+# no1 = int(input("please enter your first value"))
+# no2 = int(input("please enter your second value"))
+
+# print('logical and op? ',no1 and no2)
+# print('logical or OP ',no1 or no2)
+
+# no1 = str(input("please enter your first value"))
+# no2 = str(input("please enter your second value"))
+
+# print('logical and op ? ',no1 and no2)
+# print('logical or OP ',no1 or no2)
+
+# a = 2 
+# b = 3
+# result = (a * a**b)*((a+b)**a)/b
+# print(result)
+
+# a = int(input("please add 'a' Value "))
+# b = int(input("please add 'b' Value "))
+# c = 2
+# result = print( (a*8)**c*(a + b * c)/2 )
+
+
+# first_Name = str(input("please add your first name ")) 
+# secondName = str(input("please add your first name ")) 
+# age = int(input("please enter your age "))
+# print("Hello ",first_Name,secondName, "Welcome to our website \n","You are eligible for Addmission because", "You are",age, " Years Old")
+
+
+
+# Concstinate 
+
+# value1 = 10
+# value2 = 5
+# result3 = "check condition", value1, " and ", value2 , "conditionb: " ,
+# result4 = value1 and value2
+
+# result5 = "check condition", value1, " and ", value2 , "condition: "
+# result6 = value2 and value1
+
+# print(result3, result4,"\n", result5, result6 )
+
+
+
+
+# Quotient 
+
+# hame btata he k kitny bar is se multiply hua
+# a = 10 
+# b = 2
+# print(a // b )
+
+# c = int(input("enter your large value: "))
+# d = int(input("enter your Division value: "))
+
+# e = c // d 
+# print(e)
+
+# a = 1281 
+# b = 2.34
+# c = "Naeem"
+# d = ["naeem","hussain"]
+# e = {"a": "naeem","b":"Hussain"}
+
+# print(d[1])
+
+# f = 33,33,22,11
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+
+# print(type(e))
+# print(type(f))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
